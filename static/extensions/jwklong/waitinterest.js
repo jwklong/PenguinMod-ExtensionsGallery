fd23db2f9235c67a70920cf792ffc4d3190cb639
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-10-16 22:06:56 +0100

    Update waitinterest.js

--- static/extensions/jwklong/waitinterest.js
+++ static/extensions/jwklong/waitinterest.js
@@ -7,17 +7,29 @@ class WaitInterest {
       color2: '#4f48cf',
       blocks: [
         {
-          opcode: 'test',
+          opcode: 'setExpire',
           blockType: Scratch.BlockType.COMMAND,
-          text: 'test',
+          text: 'set expiry of [WAIT] to [TIME] milliseconds',
           disableMonitor: true,
+          arguments: {
+            WAIT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "abc"
+            },
+            TIME: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 1000
+            }
+          }
         },
       ],
     }
   }
 
-  test() {
-    
+  expiries: {}
+
+  setExpire(args) {
+    expiries[args.WAIT] = Date.now() + args.TIME
   }
 }
 Scratch.extensions.register(new WaitInterest())
